ed38f89bd928e2d28091315ca82c4bd21c64f173
Daouda Dia <diarise@gmail.com>
2014-01-24 10:38:17 -0800

    added script for scrolling filter

--- sites/all/themes/ukabbalah/js/persistheader.js
+++ sites/all/themes/ukabbalah/js/persistheader.js
@@ -3,5 +3,6 @@
 	    $('#mainOuterContentLogoMenuWrapper').toggleClass('scrolling', $(window).scrollTop() > $('#outerMenu').offset().top);
 	    $('.donateFloat').toggleClass('donateFloatFix', $(window).scrollTop() > $('#outerMenu').offset().top);
 	    $('#headerDonateButtonFloat').toggleClass('headerDonateButtonFloatAppear', $(window).scrollTop() > $('#outerMenu').offset().top);
+	    $('#teachingsSectionHeaderBar').toggleClass('scrollingFilter', $(window).scrollTop() > $('#pageregion').offset().top);
 	});
 })(jQuery);
