a82cb3ffed982c712b73dc67893f2aebe5619880
Daouda Dia <diarise@gmail.com>
2014-01-24 12:27:17 -0800

    updated css

--- sites/all/themes/ukabbalah/js/persistheader.js
+++ sites/all/themes/ukabbalah/js/persistheader.js
@@ -4,6 +4,6 @@
 	    $('.donateFloat').toggleClass('donateFloatFix', $(window).scrollTop() > $('#outerMenu').offset().top);
 	    $('#headerDonateButtonFloat').toggleClass('headerDonateButtonFloatAppear', $(window).scrollTop() > $('#outerMenu').offset().top);
 	    $('#teachingsControlSection').toggleClass('scrollingTitle', $(window).scrollTop() > $('#pageregion').offset().top);
-	    $('#teachingsSectionHeaderBar').toggleClass('scrollingFilter parent_topic', $(window).scrollTop() > $('#pageregion').offset().top);
+	    $('#teachingsSectionHeaderBar').toggleClass('scrollingFilter', $(window).scrollTop() > $('#pageregion').offset().top);
 	});
 })(jQuery);
