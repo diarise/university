1c9745db009125796d98c5239c75485d519b8703
Daouda Dia <diarise@gmail.com>
2014-01-24 13:05:50 -0800

    updated scroll script

--- sites/all/themes/ukabbalah/js/persistheader.js
+++ sites/all/themes/ukabbalah/js/persistheader.js
@@ -5,6 +5,7 @@
 	    $('#headerDonateButtonFloat').toggleClass('headerDonateButtonFloatAppear', $(window).scrollTop() > $('#outerMenu').offset().top);
 	    $('#teachingsControlSection').toggleClass('scrollingTitle', $(window).scrollTop() > $('#pageregion').offset().top);
 	    $('#teachingsSectionHeaderBar').toggleClass('scrollingFilter', $(window).scrollTop() > $('#pageregion').offset().top);
+	    $('#teachingsControlSectionInnerWrapper').toggleClass('teachingsBarInnerWrapper', $(window).scrollTop() > $('#pageregion').offset().top);
 	    $('#teachingsBarInnerWrapper').toggleClass('teachingsBarInnerWrapper', $(window).scrollTop() > $('#pageregion').offset().top);
 	});
 })(jQuery);
