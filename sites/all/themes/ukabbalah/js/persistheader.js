510af46c4b22779fdb8d4d09cce2389fdd7cae50
Daouda Dia <diarise@gmail.com>
2014-01-24 17:12:04 -0800

    roll back

--- sites/all/themes/ukabbalah/js/persistheader.js
+++ sites/all/themes/ukabbalah/js/persistheader.js
@@ -7,9 +7,9 @@
         $('#teachingsSectionHeaderBar').toggleClass('scrollingFilter', $(window).scrollTop() > $('#pageregion').offset().top);
         $('#teachingsControlSectionInnerWrapper').toggleClass('teachingsBarInnerWrapper parent_topic', $(window).scrollTop() > $('#pageregion').offset().top);
         $('#teachingsBarInnerWrapper').toggleClass('teachingsBarInnerWrapper parent_topic', $(window).scrollTop() > $('#pageregion').offset().top);
-        $('#page-title').toggleClass('scrollingTitle', $(window).scrollTop() > $('#pageregion').offset().top);
-        $('.termDesc').toggleClass('scrollingTermDesc', $(window).scrollTop() > $('#pageregion').offset().top);
-        $('.view-filters').toggleClass('scrollingView-filters', $(window).scrollTop() > $('#pageregion').offset().top);
+        // $('#page-title').toggleClass('scrollingTitle', $(window).scrollTop() > $('#pageregion').offset().top);
+        // $('.termDesc').toggleClass('scrollingTermDesc', $(window).scrollTop() > $('#pageregion').offset().top);
+        // $('.view-filters').toggleClass('scrollingView-filters', $(window).scrollTop() > $('#pageregion').offset().top);
     });
 
     var resizeTimer;
