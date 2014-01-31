458f192bda9e0875a8668e2d3f9dd32cfb479190
umesh handore <umesh.handore@kabbalah.com>
2014-01-31 14:05:28 -0800

    checking the context value

--- sites/all/themes/ukabbalah/js/activelesson.js
+++ sites/all/themes/ukabbalah/js/activelesson.js
@@ -8,6 +8,7 @@ Drupal.behaviors.myBehavior = {
 	$('.views-table tbody tr').each(function() {
 	    //alert($(this).find('.Video_link').val()); // `this` is TR DOM element
 	    var href = $(this).find('.Video_link').attr('href');
+	    console.log(context.URL);
 	     if (href == context.URL){
      	$('.Video_link span').text('NOW PLAYING');
      }
