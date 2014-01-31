93fb66d1c3d8f2f9827a64b971869fc9ba0f749a
umesh handore <umesh.handore@kabbalah.com>
2014-01-31 13:24:24 -0800

    removed the console

--- sites/all/themes/ukabbalah/js/activelesson.js
+++ sites/all/themes/ukabbalah/js/activelesson.js
@@ -8,7 +8,6 @@ Drupal.behaviors.myBehavior = {
     var href = $('.Video_link').attr('href');
     console.log(href);
     console.log(Drupal.settings);
-    console.log(Drupal.settings.nodepath.path);
      if (href == Drupal.settings.nodepath){
      	$('.Video_link span').text('NOW PLAYING');
      }
