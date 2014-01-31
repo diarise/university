fbb6b19c760ee1f87a848b5dafbc44cbff9508ef
umesh handore <umesh.handore@kabbalah.com>
2014-01-31 13:21:44 -0800

    checking the variables

--- sites/all/themes/ukabbalah/js/activelesson.js
+++ sites/all/themes/ukabbalah/js/activelesson.js
@@ -7,7 +7,8 @@ Drupal.behaviors.myBehavior = {
     //code starts
     var href = $('.Video_link').attr('href');
     console.log(href);
-    console.log(Drupal.settings.nodepath);
+    console.log(Drupal.settings);
+    console.log(Drupal.settings.nodepath.path);
      if (href == Drupal.settings.nodepath){
      	$('.Video_link span').text('NOW PLAYING');
      }
