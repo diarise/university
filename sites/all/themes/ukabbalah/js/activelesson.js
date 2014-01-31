29ccbe5cefe3e6b56a3ab44b1aa40f750de5623d
umesh handore <umesh.handore@kabbalah.com>
2014-01-31 14:00:34 -0800

    checking href.

--- sites/all/themes/ukabbalah/js/activelesson.js
+++ sites/all/themes/ukabbalah/js/activelesson.js
@@ -6,23 +6,20 @@ Drupal.behaviors.myBehavior = {
   attach: function (context, settings) {
     //code starts
     var href = $('.Video_link').attr('href');
-    console.log(href);
+  
     console.log(context.URL);
      if (href == context.URL){
      	$('.Video_link span').text('NOW PLAYING');
      }
-     var table = $('table');
-	handle(table);
-	function handle(table){
-     table.find('td.watch-width').each(function(){
-     	var href = $('.Video_link').attr('href');
-     	 if (href == context.URL){
-     	$('.Video_link span').text('NOW PLAYING');
-     }
-        
-    } );
+
+	$('.views-table tbody tr').each(function() {
+	    //alert($(this).find('.Video_link').val()); // `this` is TR DOM element
+	    var href = $(this).find('.Video_link').attr('href');
+	      console.log(href);
+	});
+
+
     //code ends
-  }
   }
 };
 })(jQuery);
