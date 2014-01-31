cf58f6434f7d4210506aa9c6ce911f6da6139df7
umesh handore <umesh.handore@kabbalah.com>
2014-01-31 14:02:57 -0800

    updated the code to change the watch now

--- sites/all/themes/ukabbalah/js/activelesson.js
+++ sites/all/themes/ukabbalah/js/activelesson.js
@@ -5,17 +5,12 @@
 Drupal.behaviors.myBehavior = {
   attach: function (context, settings) {
     //code starts
-    var href = $('.Video_link').attr('href');
-  
-    console.log(context.URL);
-     if (href == context.URL){
-     	$('.Video_link span').text('NOW PLAYING');
-     }
-
 	$('.views-table tbody tr').each(function() {
 	    //alert($(this).find('.Video_link').val()); // `this` is TR DOM element
 	    var href = $(this).find('.Video_link').attr('href');
-	      console.log(href);
+	     if (href == context.URL){
+     	$('.Video_link span').text('NOW PLAYING');
+     }
 	});
 
 
