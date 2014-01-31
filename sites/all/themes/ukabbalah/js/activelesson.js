17e9a93e56334111ccc4f3f090f10cb8969bb22e
umesh handore <umesh.handore@kabbalah.com>
2014-01-31 15:00:39 -0800

    getting the class name of child element

--- sites/all/themes/ukabbalah/js/activelesson.js
+++ sites/all/themes/ukabbalah/js/activelesson.js
@@ -10,11 +10,10 @@ Drupal.behaviors.myBehavior = {
 	$('.views-table tbody tr').each(function() {
 	    //alert($(this).find('.Video_link').val()); // `this` is TR DOM element
 	    var href = $(this).find('.Video_link').attr('href');
-	  
-	    
 	  if (href == context.URL){
-	  	  console.log(context.URL);
-	  	   console.log(href);
+	  	var current = $('.watch-width').find('a.Video_link').child().attr('class');
+	  	 console.log(current);
+	  	 console.log(href);
 	 	 className = $('.Video_link span').attr('class');
 	 	console.log(className);
     	}
