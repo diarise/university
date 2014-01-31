34d40826b559d513294a71bb64654bd9763cddf5
umesh handore <umesh.handore@kabbalah.com>
2014-01-31 15:17:20 -0800

    cleaned the script

--- sites/all/themes/ukabbalah/js/activelesson.js
+++ sites/all/themes/ukabbalah/js/activelesson.js
@@ -8,7 +8,6 @@ Drupal.behaviors.myBehavior = {
     var className;
 
 	$('.views-table tbody tr').each(function() {
-	    //alert($(this).find('.Video_link').val()); // `this` is TR DOM element
 	    var href = $(this).find('.Video_link').attr('href');
 	  if (href == context.URL){
 	 	 className = $(this).find('.Video_link span').attr('class');
@@ -16,7 +15,6 @@ Drupal.behaviors.myBehavior = {
     	}
     
 	});
-		//$("'.'.className").text('NOW PLAYING');
     //code ends
   }
 };
