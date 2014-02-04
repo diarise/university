a50a3aeae44f4384ba96b4b283d13f403789825b
Daouda Dia <diarise@gmail.com>
2014-02-04 10:50:32 -0800

    added script Web Analytics Tracker

--- sites/all/themes/ukabbalah/html.tpl.php
+++ sites/all/themes/ukabbalah/html.tpl.php
@@ -21,6 +21,25 @@
   <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
+  <!-- Start Open Web Analytics Tracker -->
+  <script type="text/javascript">
+  //<![CDATA[
+  var owa_baseUrl = 'http://localhost/owa/';
+  var owa_cmds = owa_cmds || [];
+  owa_cmds.push(['setSiteId', 'ca828ab86bab4c4c67ed5bc67db80bad']);
+  owa_cmds.push(['trackPageView']);
+  owa_cmds.push(['trackClicks']);
+  owa_cmds.push(['trackDomStream']);
+
+  (function() {
+   var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
+   owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
+   _owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
+   var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+  }());
+  //]]>
+  </script>
+  <!-- End Open Web Analytics Code -->
 </head>
 
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
