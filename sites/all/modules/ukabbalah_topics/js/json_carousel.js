6fd9342782dbf0f698fa416c9285617ba447b61d
Rohan D'Souza <rohan.dsouza@kabbalah.com>
2014-02-04 12:31:07 -0800

    testing hover

--- sites/all/modules/ukabbalah_topics/js/json_carousel.js
+++ sites/all/modules/ukabbalah_topics/js/json_carousel.js
@@ -119,38 +119,38 @@ var Slide = function(elem, options){
                     });
                     setSlideContent.html(rowContent);
 
-                    // $.each($(".eachCont"), function(){
-                        // titleHeight = $(this).find(".theTitle").outerHeight();
-                        // topicHeight = $(this).find(".teachingsTopicAndDate").outerHeight();
+                    $.each($(".eachCont"), function(){
+                        titleHeight = $(this).find(".theTitle").outerHeight();
+                        topicHeight = $(this).find(".teachingsTopicAndDate").outerHeight();
                     
-                        // $(this).find(".theDescCont").css ({
-                            // "margin-top": -(titleHeight + topicHeight + 14),
-                        // });                                
+                        $(this).find(".theDescCont").css ({
+                            "margin-top": -(titleHeight + topicHeight + 14),
+                        });                                
 
-                        // $(this).find(".imagesCont").hover(         //hove effect of the each image                                                
-                            // function () {
-                                // titleHeight = $(this).find(".theTitle").outerHeight();
-                                // topicHeight = $(this).find(".teachingsTopicAndDate").outerHeight();
+                        $(this).find(".imagesCont").hover(         //hove effect of the each image                                                
+                            function () {
+                                titleHeight = $(this).find(".theTitle").outerHeight();
+                                topicHeight = $(this).find(".teachingsTopicAndDate").outerHeight();
 
-                                // $(this).find(".theDescCont").stop().animate({ 
-                                    // marginTop: -180,
-                                   // // paddingTop:20,
-                                // }, 350);
+                                $(this).find(".theDescCont").stop().animate({ 
+                                    marginTop: -180,
+                                   // paddingTop:20,
+                                }, 350);
 
-                            // },
+                            },
 
-                            // function () {
-                                // titleHeight = $(this).find(".theTitle").outerHeight();
-                                // topicHeight = $(this).find(".teachingsTopicAndDate").outerHeight();
+                            function () {
+                                titleHeight = $(this).find(".theTitle").outerHeight();
+                                topicHeight = $(this).find(".teachingsTopicAndDate").outerHeight();
                                 
-                                // $(this).find(".theDescCont").stop().animate({ 
-                                    // marginTop: -(titleHeight + topicHeight + 14),
-                                  // //  paddingTop:0,
-                                // }, 350);    
-                            // }
-                        // );
+                                $(this).find(".theDescCont").stop().animate({ 
+                                    marginTop: -(titleHeight + topicHeight + 14),
+                                  //  paddingTop:0,
+                                }, 350);    
+                            }
+                        );
 
-                    // });
+                    });
                 }
                 startPoint();
             },  // end of success function           
--- sites/all/themes/ukabbalah/js/hover.js
+++ sites/all/themes/ukabbalah/js/hover.js
@@ -5,38 +5,7 @@
         var titleHeight = 0;
         var dateHeight = 0;
 
-        // $.each($(".masonry li"), function(){
-          // titleHeight = $(this).find(".theJsonTitle").outerHeight();
-          // dateHeight = $(this).find(".teachingsTopicAndDate").outerHeight();
-      
-          // $(this).find(".theDescCont").css ({
-              // "margin-top": -(titleHeight + dateHeight + 13),
-          // }); 
-
-          // $(this).hover(
-
-          // function(){
-
-            // $(this).find(".theDescCont").stop().animate({
-             // marginTop: "-180px",
-            // },220);
-
-           // },
-
-           // function(){
-
-            // titleHeight = $(this).find(".theJsonTitle").outerHeight();
-            // dateHeight = $(this).find(".teachingsTopicAndDate").outerHeight();
-
-            // $(this).find(".theDescCont").stop().animate({
-             // "margin-top": -(titleHeight + dateHeight + 13),
-            // },220);
-          // })
-        // });
-		
-		
-		
-		$.each($(".eachCont"), function(){
+        $.each($(".masonry li"), function(){
           titleHeight = $(this).find(".theJsonTitle").outerHeight();
           dateHeight = $(this).find(".teachingsTopicAndDate").outerHeight();
       
@@ -64,10 +33,6 @@
             },220);
           })
         });
-		
-		
-		
-		
 		
 });
 
