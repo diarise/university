79bdb1b187f49aa8bcbdd3efb976d47d0198c354
Daouda Dia <diarise@gmail.com>
2014-02-07 11:35:37 -0800

    trying to identify active lesson

--- sites/all/themes/ukabbalah/js/activelesson.js
+++ sites/all/themes/ukabbalah/js/activelesson.js
@@ -13,6 +13,7 @@ Drupal.behaviors.myBehavior = {
 	  if (videohref == context.URL){
 	 	 $(this).find('.Video_link span').text('NOW PLAYING');
 	 	 $(this).find('.Video_link span').addClass( "nowplaying" );
+	 	 $(this).find('td').addClass( "viewed" );
     	}
     	
     	 if (audiohref == context.URL){
