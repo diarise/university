1fc38dda4f0e9b6928db6b3d01900ce671d83316
Daouda Dia <diarise@gmail.com>
2014-03-06 13:16:20 -0800

    node testing

--- sites/all/themes/ukabbalah/node--live-events.tpl.php
+++ sites/all/themes/ukabbalah/node--live-events.tpl.php
@@ -1,7 +1,32 @@
-<?php
+<div id="wrapperVideoSection">
+	<div class="wrapperCours">
+		<div id="contentAuthorImages">
+			<img src="<?php print $node->field_image_cdn_link['und'][0]['value']; ?>">
+		</div>
+		<div id="contentAuthorInfo">
+				<div id="wrapperInfoCour">
+					<span id="titleCourse"><?php print $node->title; ?> </span>
+										<span id="topicCourseTitle"> Topic:</span>
+					<span id="topicCourse"> 
+					<a href="/primary-topics/kabbalistic-concepts">topic text</a>					</span>
+					<span class="theCourseAuthorTitle">Teacher:
+						<span class="theCourseAuthor">
+						  <a class="Eitan Yardeni" href="javascript:void(0)">teacher text</a>	
+						</span>
+					</span>
+					
+				</div>
+		
+				<div id="wrapperPrice">
+						<span class="buttonMember"><a href="#">become a member</a></span>
+						<span class="priceLogin"><a href="#">log-in</a></span>
+				</div>
+			
+			
+			
+		</div>
+	</div>
+</div>
 
-// print_r( $node );
 
-// print $node->title;
-// print $node->field_image_cdn_link['und'][0]['value'];
-// print $node->field_streamer_link['und'][0]['value'];
+
