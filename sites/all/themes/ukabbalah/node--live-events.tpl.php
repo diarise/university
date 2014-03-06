50e5b76e0d81b7b695912ecb2c9b88d971f2529f
Rohan D'Souza <rohan.dsouza@kabbalah.com>
2014-03-06 13:07:57 -0800

    test

--- sites/all/themes/ukabbalah/node--live-events.tpl.php
+++ sites/all/themes/ukabbalah/node--live-events.tpl.php
@@ -1,3 +1,7 @@
 <?php
 
-print_r( $node );
+// print_r( $node );
+
+// print $node->title;
+// print $node->field_image_cdn_link['und'][0]['value'];
+// print $node->field_streamer_link['und'][0]['value'];
