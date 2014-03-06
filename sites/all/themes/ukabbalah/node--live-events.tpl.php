862e118c2fa4acaf564b23cc96ed20ada9ebea60
Daouda Dia <diarise@gmail.com>
2014-03-06 15:42:48 -0800

    updated node for log in session

--- sites/all/themes/ukabbalah/node--live-events.tpl.php
+++ sites/all/themes/ukabbalah/node--live-events.tpl.php
@@ -1,4 +1,186 @@
-<div id="wrapperVideoSection">
+<!-- test log in  -->
+
+<?php if( user_is_logged_in() ) {?>
+
+<div id="wrapperVideoSection"><!--start video wrapper section-->
+	<?php } 
+	if (user_is_logged_in() && get_membership_based_content_body( $node->nid ) ) {?>	
+
+	<div class="videocontent"><!--start of video content when user log in-->
+		<?php
+			
+			if( $field_type_of_lesson == 'Audio' ) {
+		?>
+			<div id="audio-control"> 
+				<?php print render($content['field_audio_link']); ?>
+			</div>
+		<?php 
+				} else if( $field_type_of_lesson == 'Video' ) { 
+		?>
+		<div id="videoSocialIconeWrapper">
+			<div id="video-control"> 
+				<?php print render($content['field_lesson_video']);  } ?>
+			</div>
+			<div id="eSocial"> <!--start of social media icone-->
+				<div class="sharethis">
+					<span class='st_facebook_large' displayText='Facebook'></span>
+					<span class='st_twitter_large' displayText='Tweet' st_via='kabbalahcentre'></span>
+					<span class='st_googleplus_large' displayText='Google +'></span>
+					<span class='st_pinterest_large' displayText='Pinterest'></span>
+					<span class='st_email_large' displayText='Email'></span>
+					<span class='st_sharethis_large' displayText='ShareThis'></span>
+				</div>
+			</div> <!--start of social media icone-->
+		</div>	
+	</div><!--end of of video content when user log in-->
+	<?php  } else {
+		
+		$course_nid= node_load($node->field_course_list['und'][0]['node']->nid);
+		?>
+		
+		<div class="preVideoImg"><!--start preview video for user log out-->
+			<?php if( $field_type_of_lesson == 'Video' ) {?>
+		<img src="http://twistassets.kabbalah.com/videos/<?php print $node->field_lesson_video[und][0][twistage_existing_videos];?>/screenshots/620w.jpg"  > <!--image for video preview-->
+		<?php } else{ ?>
+			<img src="<?php print $course_nid->field_image_cdn_link['und'][0]['value']; ?>"  /> <!--image for video preview-->
+			<?php } ?>
+		
+		<div class="videotitles"><!--start of preview video description-->
+			<div id="wrapperInfoMultimedia">
+				<span id="titleMultimedia"><?php print $title; // Title ?></span>
+				<span id="courseNameMultimedia"><?php  print $course_title;	?></span>
+				<span class="pipe">|</span>
+				<span id="primaryTopicMultimedia">
+				<?php	
+					echo $primary_topic;
+					//echo implode( ", " , $secondary_parent_topics ); 
+				?>
+				</span>
+			</div>
+			<div id="wrapperAuthorDate">
+				<span id="authorNameMultimedia">
+				<?php	echo implode( ", " , $authors_name ); // Author name 	?>
+				</span>
+				<span id="dateMultimedia">
+				<?php
+					if( sizeof($node->field_recorded_date) > 0 ) print date('F jS, Y ',strtotime($node->field_recorded_date['und'][0]['value'])); // Date Node Changed
+				?>
+				</span>
+			</div>
+		</div><!--end of preview video description-->
+		<div id="wrapperPrice"><!--start of log in-->
+			<?php if( user_is_logged_in() ) {?>
+				<span class="buttonMember"><a href="#">upgrade your membership</a></span>
+			<?php } else { ?>
+				<span class="buttonMember"><a href="#">become a member</a></span>
+				<span class="priceLogin"><a href="#">log-in</a></span>
+			<?php } ?>
+		</div><!--end of log in-->
+		<div class="sharethis"><!--start of social media icone-->
+<span class="bookmark">
+<?php print flag_create_link('bookmarks', $node->nid); ?>
+</span>
+			<span class='st_facebook_large' displayText='Facebook'></span>
+			<span class='st_twitter_large' displayText='Tweet' st_via='kabbalahcentre'></span>
+			<span class='st_googleplus_large' displayText='Google +'></span>
+			<span class='st_pinterest_large' displayText='Pinterest'></span>
+			<span class='st_email_large' displayText='Email'></span>
+			<span class='st_sharethis_large' displayText='ShareThis'></span>
+		</div><!--start of social media icone-->
+	</div><!--end preview video for user log out-->
+		
+	<?php }	?>
+	
+</div><!--end of video wrapper section-->
+
+	<div id="videoTitlesWrapper"> <!--strat for video description when user log in-->
+		<div class="videotitles">
+			<div id="wrapperInfoMultimedia">
+				<span id="titleMultimedia"><?php print $title; // Title ?></span>
+				<span id="courseNameMultimedia"><?php  print $course_title;	?></span>
+				<span class="pipe">|</span>
+				<span id="primaryTopicMultimedia">
+				<?php	
+					echo $primary_topic;
+					//echo implode( ", " , $secondary_parent_topics ); 
+				?>
+				</span>
+			</div>
+			<div id="wrapperAuthorDate">
+				<span id="authorNameMultimedia">
+				<?php echo implode( ", " , $authors_name ); // Author name ?>
+				</span>
+				<span id="dateMultimedia">
+				<?php
+					if( sizeof($node->field_recorded_date) > 0 ) print date('F jS, Y',strtotime($node->field_recorded_date['und'][0]['value'])); // Date Node Changed
+				?>
+				</span>
+			</div>
+			<span class="bookmark">
+				<?php print flag_create_link('bookmarks', $node->nid); ?>
+			</span>
+		</div>
+	</div><!--end of video description when user log in-->
+
+
+
+
+
+
+
+
+<div class="videoContent">
+		
+		<!-- Class Description -->
+		<?php 
+		
+		if( sizeof( $node->body) > 0  ) {
+		if( $node->body['und'][0]['value'] != "" ) { ?>
+			<div id="classesDescription">
+				<input type="checkbox" id="ac-0">
+				<label  class="titleDescription" for="ac-0">events description</label>
+				<span class="accordion"><?php  	print $node->body['und'][0]['value']; ?></span>
+			</div>
+		<?php } 
+		}
+		?>
+		
+		<!-- Class Highlights -->
+		<?php if( sizeof( $node->field_lesson_highlights) > 0  ) { ?>
+		<?php if( $node->field_lesson_highlights['und'][0]['value'] != "" ) { ?>
+		<div id="classHighlights">
+			<input type="checkbox" id="ac-1">
+			<label class="titleDescription" for="ac-1">Highlights</label>
+			<span class="accordion"><?php print $node->field_lesson_highlights['und'][0]['value']; ?></span>
+		</div>
+		<?php } ?>
+		<?php } ?>
+		
+		<!-- Class Rsources -->
+		<?php if( sizeof( $node->field_lesson_resources) > 0  ) { ?>
+		<div class="titleDescription">Resources</div>
+		<div id="classResources">
+			<?php
+				// This is a way to display multiple entries for field collections 
+				$wrapper = entity_metadata_wrapper('node', $node);
+				 foreach ($wrapper->field_lesson_resources as $i)
+				 {
+					print "<a href='".$i->field_file_link->value()."' target='_blank' ><span class='resourceFile'></span></a>";
+					print "<span class='resourceTitle'><a href='".$i->field_file_link->value()."' target='_blank' >".$i->field_file_description->value()."</a></span>";
+				 }
+			?>
+		</div>	
+		<?php } ?>
+		<?php if($secondary_parent_topics) { ?>
+		<div class='keywords'><?php echo implode( ", " , $secondary_parent_topics ); ?></div>		
+		<?php } ?>
+	</div><!--end of videocontent-->
+
+
+	<?php } else { ?>
+
+
+		<div id="wrapperVideoSection">
 	<div class="wrapperCours">
 		<div id="contentAuthorImages">
 			<img src="<?php print $node->field_image_cdn_link['und'][0]['value']; ?>">
@@ -54,66 +236,6 @@
 	</div>
 </div>
 
-
-
-
-
-
-
-
-<!-- test log in  -->
-
-<?php if( user_is_logged_in() ) {?>
-
-<div class="videoContent">
-		
-		<!-- Class Description -->
-		<?php 
-		
-		if( sizeof( $node->body) > 0  ) {
-		if( $node->body['und'][0]['value'] != "" ) { ?>
-			<div id="classesDescription">
-				<input type="checkbox" id="ac-0">
-				<label  class="titleDescription" for="ac-0">events description</label>
-				<span class="accordion"><?php  	print $node->body['und'][0]['value']; ?></span>
-			</div>
-		<?php } 
-		}
-		?>
-		
-		<!-- Class Highlights -->
-		<?php if( sizeof( $node->field_lesson_highlights) > 0  ) { ?>
-		<?php if( $node->field_lesson_highlights['und'][0]['value'] != "" ) { ?>
-		<div id="classHighlights">
-			<input type="checkbox" id="ac-1">
-			<label class="titleDescription" for="ac-1">Highlights</label>
-			<span class="accordion"><?php print $node->field_lesson_highlights['und'][0]['value']; ?></span>
-		</div>
-		<?php } ?>
-		<?php } ?>
-		
-		<!-- Class Rsources -->
-		<?php if( sizeof( $node->field_lesson_resources) > 0  ) { ?>
-		<div class="titleDescription">Resources</div>
-		<div id="classResources">
-			<?php
-				// This is a way to display multiple entries for field collections 
-				$wrapper = entity_metadata_wrapper('node', $node);
-				 foreach ($wrapper->field_lesson_resources as $i)
-				 {
-					print "<a href='".$i->field_file_link->value()."' target='_blank' ><span class='resourceFile'></span></a>";
-					print "<span class='resourceTitle'><a href='".$i->field_file_link->value()."' target='_blank' >".$i->field_file_description->value()."</a></span>";
-				 }
-			?>
-		</div>	
-		<?php } ?>
-		<?php if($secondary_parent_topics) { ?>
-		<div class='keywords'><?php echo implode( ", " , $secondary_parent_topics ); ?></div>		
-		<?php } ?>
-	</div><!--end of videocontent-->
-
-
-	<?php } else { ?>
 
 
 
