1777bfe7eb3a8dc7bf8735397144c6be2c8a993a
Daouda Dia <diarise@gmail.com>
2014-03-07 10:12:54 -0800

    added region on the live event tpl file, updated info file

--- sites/all/themes/ukabbalah/page--live-events.tpl.php
+++ sites/all/themes/ukabbalah/page--live-events.tpl.php
@@ -52,6 +52,9 @@
 		<?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper"><?php print render($tabs); ?></div><?php endif; ?>
 		<?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
 		<?php print render($page['content']); ?>
+		<div class="liveEventCollectionSection">
+		<?php print render($page['liveEventCollectionSection']); ?>
+		</div>
 		</div><!--end of maincontent-->
 	</div><!-- end of pageregion -->
   
