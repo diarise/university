ceed3450bf91267514b3a50eac8221c3292b2fbf
Daouda Dia <diarise@gmail.com>
2014-03-07 14:02:53 -0800

    updated pop up

--- sites/all/themes/ukabbalah/js/author_tooltip.js
+++ sites/all/themes/ukabbalah/js/author_tooltip.js
@@ -69,6 +69,8 @@ $(document).ready(function(){
 			trigger: "custom",
 			content: "loading...",
 			position: "right",
+			offsetY: -400,
+			offsetX: 350,
 			functionBefore: function(origin, continueTooltip) {				
 				continueTooltip();
 
