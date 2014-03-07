fc4c100f591eb062cbc76abe11a1aa55385ec1ce
Daouda Dia <diarise@gmail.com>
2014-03-06 17:39:09 -0800

    updated node

--- sites/all/themes/ukabbalah/node--live-events.tpl.php
+++ sites/all/themes/ukabbalah/node--live-events.tpl.php
@@ -49,7 +49,7 @@
 				<span id="titleMultimedia"><?php print $title; // Title ?></span>
 				<span id="courseNameMultimedia"><?php
 							$authors = _taxonomy_node_get_terms_by_vocabulary($node, 7 );
-							foreach ( $authors as $author ) {	echo "  <a href='javascript:void(0)' class = '".$author->name."'>" .$author->name. "</a><span class='divider'> - </span>"; }	
+							foreach ( $authors as $author ) {	echo "  <span>" .$author->name. "</span><span class='divider'> - </span>"; }	
 						?></span>
 				
 			</div>
