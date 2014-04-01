570a65bd7b9ad6f574b2d9c1273b71ad020f8235
umesh handore <umesh.handore@kabbalah.com>
2014-04-01 14:02:05 -0700

    updated the access logic

--- sites/all/themes/ukabbalah/node--lesson.tpl.php
+++ sites/all/themes/ukabbalah/node--lesson.tpl.php
@@ -19,8 +19,6 @@
 	$membership_terms = _taxonomy_node_get_terms_by_vocabulary($node, 12 );
 	foreach( $membership_terms as $t )	{	$membership = $t->name; }
 	
-	print_r($membership);
-	
 	$title = $node->title;
 	
 	if( sizeof( $node->field_course_list) > 0  )
@@ -44,7 +42,7 @@
 <?  } else if( $field_type_of_lesson == 'Video' ) { ?>
 <div id="wrapperVideoSection"><!--start video wrapper section-->
 	<?php } 
-	if (user_is_logged_in() && get_membership_based_content_body( $node->nid ) ) {?>	
+	if ((user_is_logged_in() && get_membership_based_content_body( $node->nid )) || ($membership=="") ) {?>	
 
 	<div class="videocontent"><!--start of video content when user log in-->
 		<?php
