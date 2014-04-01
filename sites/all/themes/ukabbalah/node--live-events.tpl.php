1f16bd874e14a5cbb92e1efc8e2eb5d877fd6fdd
umesh handore <umesh.handore@kabbalah.com>
2014-04-01 13:16:39 -0700

    print the vocab terms

--- sites/all/themes/ukabbalah/node--live-events.tpl.php
+++ sites/all/themes/ukabbalah/node--live-events.tpl.php
@@ -21,6 +21,7 @@
 	
 	
 		$vocab_terms = _taxonomy_node_get_terms_by_vocabulary($node, 12 );
+		print_r($vocab_terms);
 		foreach( $vocab_terms as $t )	{
 			print($t->name);
 				print ($article_membership = $t->name); }
