d0442f97daa0a96200586ae462825712a6246f28
Daouda Dia <diarise@gmail.com>
2014-04-03 10:41:34 -0700

    switched beginners courses to recently added regions on the home page

--- sites/all/themes/ukabbalah/page--front.tpl.php
+++ sites/all/themes/ukabbalah/page--front.tpl.php
@@ -48,13 +48,13 @@
 	</div> <!--end of slideshow-->
 	
 	
-	<div id="carousal1Wrapper">
-		<?php  print render($page['fpbeginnerscourses']);?>
-	</div> <!--end of carousal1-->
-	
 	<div id="carousal2Wrapper">
 		<?php  print render($page['fprecentlyadded']);?>
 	</div> <!--end of carousal2-->
+
+	<div id="carousal1Wrapper">
+		<?php  print render($page['fpbeginnerscourses']);?>
+	</div> <!--end of carousal1-->
 	
 	<div id="carousal3Wrapper">
 		<?php  print render($page['fprecommendedcourses']);?>
