7d51d717934ad8f99063e89250f947d400833fb0
Daouda Dia <diarise@gmail.com>
2014-04-28 10:42:56 -0700

    updated HTML for best validation

--- sites/all/themes/ukabbalah/node--lesson.tpl.php
+++ sites/all/themes/ukabbalah/node--lesson.tpl.php
@@ -182,7 +182,7 @@
 			<div id="classesDescription">
 				<input type="checkbox" id="ac-0">
 				<label  class="titleDescription" for="ac-0">class description</label>
-				<span class="accordion"><?php  	print $node->body['und'][0]['value']; ?></span>
+				<div class="accordion"><?php  	print $node->body['und'][0]['value']; ?></div>
 			</div>
 		<?php } 
 		}
@@ -194,7 +194,7 @@
 		<div id="classHighlights">
 			<input type="checkbox" id="ac-1">
 			<label class="titleDescription" for="ac-1">Highlights</label>
-			<span class="accordion"><?php print $node->field_lesson_highlights['und'][0]['value']; ?></span>
+			<div class="accordion"><?php print $node->field_lesson_highlights['und'][0]['value']; ?></div>
 		</div>
 		<?php } ?>
 		<?php } ?>
