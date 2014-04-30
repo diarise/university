ebfec749a41bf1f698bfa5125218cbefd1f66d6f
Daouda Dia <diarise@gmail.com>
2014-04-30 12:19:55 -0700

    Flopped the recommendation and beginner's course region

--- sites/all/themes/ukabbalah/page--front.tpl.php
+++ sites/all/themes/ukabbalah/page--front.tpl.php
@@ -7,13 +7,13 @@
 		<?php  print render($page['fprecentlyadded']);?>
 	</div> <!--end of carousal2-->
 
-	<div id="carousal1Wrapper">
-		<?php  print render($page['fpbeginnerscourses']);?>
-	</div> <!--end of carousal1-->
-	
 	<div id="carousal3Wrapper">
 		<?php  print render($page['fprecommendedcourses']);?>
 	</div> <!--end of carousal3-->
+
+	<div id="carousal1Wrapper">
+		<?php  print render($page['fpbeginnerscourses']);?>
+	</div> <!--end of carousal1-->
 	
 	<div id="carousal4Wrapper">
 		<?php  print render($page['fpfreelessons']);?>
