420da5f86296ede03ef1267b8f6b307b83e9ab98
Daouda Dia <diarise@gmail.com>
2014-06-10 11:45:30 -0700

    cleaning up script

--- sites/all/themes/ukabbalah/js/persistheader.js
+++ sites/all/themes/ukabbalah/js/persistheader.js
@@ -9,7 +9,7 @@
 	});
 	
 	$(window).scroll(function () {
-        $('#mainOuterContentLogoMenuWrapper').toggleClass('scrolling', $(window).scrollTop() > $('#outerMenu').offset().top);
+        //$('#mainOuterContentLogoMenuWrapper').toggleClass('scrolling', $(window).scrollTop() > $('#outerMenu').offset().top);
         $('#menuLocalWrapper').toggleClass('scrolling', $(window).scrollTop() > $('#outerMenu').offset().top);
         //$('.donateFloat').toggleClass('donateFloatFix', $(window).scrollTop() > $('#outerMenu').offset().top);
         //$('#headerDonateButtonFloat').toggleClass('headerDonateButtonFloatAppear', $(window).scrollTop() > $('#outerMenu').offset().top);
