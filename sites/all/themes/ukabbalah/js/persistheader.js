0b236b5a51bc95852ecea796f7ca204ccdf81a2d
Daouda Dia <diarise@gmail.com>
2014-06-10 11:41:42 -0700

    cleaning js

--- sites/all/themes/ukabbalah/js/persistheader.js
+++ sites/all/themes/ukabbalah/js/persistheader.js
@@ -11,12 +11,12 @@
 	$(window).scroll(function () {
         $('#mainOuterContentLogoMenuWrapper').toggleClass('scrolling', $(window).scrollTop() > $('#outerMenu').offset().top);
         $('#menuLocalWrapper').toggleClass('scrolling', $(window).scrollTop() > $('#outerMenu').offset().top);
-        $('.donateFloat').toggleClass('donateFloatFix', $(window).scrollTop() > $('#outerMenu').offset().top);
-        $('#headerDonateButtonFloat').toggleClass('headerDonateButtonFloatAppear', $(window).scrollTop() > $('#outerMenu').offset().top);
-        $('#teachingsControlSection').toggleClass('scrollingTitle', $(window).scrollTop() > $('#pageregion').offset().top);
-        $('#teachingsSectionHeaderBar').toggleClass('scrollingFilter', $(window).scrollTop() > $('#pageregion').offset().top);
-        $('#teachingsControlSectionInnerWrapper').toggleClass('teachingsBarInnerWrapper parent_topic', $(window).scrollTop() > $('#pageregion').offset().top);
-        $('#teachingsBarInnerWrapper').toggleClass('teachingsBarInnerWrapper parent_topic', $(window).scrollTop() > $('#pageregion').offset().top);
+        //$('.donateFloat').toggleClass('donateFloatFix', $(window).scrollTop() > $('#outerMenu').offset().top);
+        //$('#headerDonateButtonFloat').toggleClass('headerDonateButtonFloatAppear', $(window).scrollTop() > $('#outerMenu').offset().top);
+        //$('#teachingsControlSection').toggleClass('scrollingTitle', $(window).scrollTop() > $('#pageregion').offset().top);
+        //$('#teachingsSectionHeaderBar').toggleClass('scrollingFilter', $(window).scrollTop() > $('#pageregion').offset().top);
+        //$('#teachingsControlSectionInnerWrapper').toggleClass('teachingsBarInnerWrapper parent_topic', $(window).scrollTop() > $('#pageregion').offset().top);
+        //$('#teachingsBarInnerWrapper').toggleClass('teachingsBarInnerWrapper parent_topic', $(window).scrollTop() > $('#pageregion').offset().top);
         // $('#page-title').toggleClass('scrollingTitle', $(window).scrollTop() > $('#pageregion').offset().top);
         // $('.termDesc').toggleClass('scrollingTermDesc', $(window).scrollTop() > $('#pageregion').offset().top);
         // $('.view-filters').toggleClass('scrollingView-filters', $(window).scrollTop() > $('#pageregion').offset().top);
