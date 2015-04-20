93d5b421fd2e560186bf090c66ca4ede8123c5e4
Daouda Dia <diarise@gmail.com>
2015-04-20 16:27:53 -0700

    updated

--- sites/all/themes/ukabbalah/page--lesson.tpl.php
+++ sites/all/themes/ukabbalah/page--lesson.tpl.php
@@ -315,7 +315,7 @@
 							<label>course: </label> 
 							<?php  
 									//$course_path = url(drupal_get_path_alias('node/' . $node->field_course_list['und'][0]['node']->nid), array('absolute' => TRUE));
-									print "<a href='".$course_path."'>".$course_title."</a>";	
+									print "<a class='coursTitleBold' href='".$course_path."'>".$course_title."</a>";	
 							?>
 						</div>
 						<div class="lessonDetailsInfo">
