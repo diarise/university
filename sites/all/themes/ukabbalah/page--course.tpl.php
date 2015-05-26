6bb086977d8af461be77b088e688b53f1b43eb90
rohan.dsouza <rohan.dsouza@kabbalah.com>
2015-05-26 12:22:22 -0700

    added global $user;

--- sites/all/themes/ukabbalah/page--course.tpl.php
+++ sites/all/themes/ukabbalah/page--course.tpl.php
@@ -1,5 +1,6 @@
    <main id="pageregion">
-		<?php print $messages; ?>
+		<?php print $messages; 
+		global $user;?>
 		<?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper"><?php print render($tabs); ?></div><?php endif; ?>
 		<?php //if ($title): ?><!--<h1 class="title" id="page-title">--><?php //print $title; ?><!--</h1>--><?php //endif; ?>
 		<?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
