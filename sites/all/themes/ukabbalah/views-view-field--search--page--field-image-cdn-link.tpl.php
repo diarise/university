5e6f079dfe9c47463cdb38467bad1f56bfeda604
rohandsouza <rohan.dsouza@kabbalah.com>
2015-06-24 12:11:29 -0700

    added a field rewirte for image cdn link for search view

---
+++ sites/all/themes/ukabbalah/views-view-field--search--page--field-image-cdn-link.tpl.php
@@ -0,0 +1,19 @@
+<?php 
+
+	
+	if( $row->_field_data['nid']['entity']->type == "lesson")
+	{	
+		if( $row->_field_data['nid']['entity']->field_type_of_lesson['und'][0]['tid']== 554 ) 
+		{ 
+			if( sizeof( $row->_field_data['nid']['entity']->field_course_list) > 0  ) $course_nid = node_load($row->_field_data['nid']['entity']->field_course_list['und'][0]['nid']); 
+			if( sizeof( $row->_field_data['nid']['entity']->field_event_list) > 0  )  $course_nid = node_load($row->_field_data['nid']['entity']->field_event_list['und'][0]['nid']); 	
+			$output = $course_nid->field_image_cdn_link['und'][0]['value'];
+		}
+		
+	}
+	
+	if($output)	print "<div class = 'searchimage'><img src ='".$output."' class = 'theImage'  alt=''/>";
+	else print $output;	
+	
+	
+?>
