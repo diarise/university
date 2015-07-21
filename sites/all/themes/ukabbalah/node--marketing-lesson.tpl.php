0cdd5d969d9fb49c21d7616249336fc0344c9d9f
rohandsouza <rohan.dsouza@kabbalah.com>
2015-07-21 12:07:24 -0700

    testing marketing lesson node

--- sites/all/themes/ukabbalah/node--marketing-lesson.tpl.php
+++ sites/all/themes/ukabbalah/node--marketing-lesson.tpl.php
@@ -2,9 +2,12 @@
 	
 	print "HELLO WORLD ____________";
 	
+	
 	// Video or Audio Display
 	foreach ($node->field_type_of_lesson as $term) 	{ 	$field_type_of_lesson = $term[0]['taxonomy_term']->name; }
 
+	print $field_type_of_lesson." Test";
+	
 	if( $field_type_of_lesson == 'Audio' ) { 
 
 	print render($content['field_audio_link']);
