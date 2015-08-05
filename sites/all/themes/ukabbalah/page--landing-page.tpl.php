d4c253daa53e7e2c70b4d6d0ff5502511ae77a1d
rohandsouza <rohan.dsouza@kabbalah.com>
2015-08-05 15:44:45 -0700

    added a tpl for landing page

---
+++ sites/all/themes/ukabbalah/page--landing-page.tpl.php
@@ -0,0 +1,12 @@
+<main id="pageregion">
+   	<div id="mainContentWrapper">
+	test
+    <?php print $messages; ?>
+    
+    <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper"><?php print render($tabs); ?></div><?php endif; ?>
+  
+    <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+    <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+    <?php print render($page['content']); ?>
+    </div><!--end of mainContentWrapper-->
+ </main><!-- end of pageregion -->
