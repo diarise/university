04340df1cbd3dc27ac95243896fcfac28c785e0f
Daouda Dia <diarise@gmail.com>
2015-08-06 10:13:21 -0700

    updated share this code

--- sites/all/themes/ukabbalah/page--landing-page.tpl.php
+++ sites/all/themes/ukabbalah/page--landing-page.tpl.php
@@ -12,12 +12,10 @@
 		<div id="journeyContentWrpper">
 			<div id="journeyContentText">
 				<div class="sharethisWrapperLanding">
-					<span class='st_sharethis_large' displayText='ShareThis'></span>
-					<span class='st_facebook_large' displayText='Facebook'></span>
-					<span class='st_twitter_large' displayText='Tweet'></span>
-					<span class='st_linkedin_large' displayText='LinkedIn'></span>
-					<span class='st_pinterest_large' displayText='Pinterest'></span>
-					<span class='st_email_large' displayText='Email'></span>
+					<span class='st_sharethis_hcount' displayText='ShareThis'></span>
+					<span class='st_facebook_hcount' displayText='Facebook'></span>
+					<span class='st_twitter_hcount' displayText='Tweet'></span>
+					<span class='st_email_hcount' displayText='Email'></span>
 				</div>
 				<p>
 					<img class="KarenImg" src="/sites/all/themes/ukabbalah/images/karen_berg-journey.jpg" alt="">
