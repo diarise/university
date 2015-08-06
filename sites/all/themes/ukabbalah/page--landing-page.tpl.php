38608202f671ad3ebeb841840b2343af7a72a423
Daouda Dia <diarise@gmail.com>
2015-08-06 10:52:20 -0700

    updated html and css

--- sites/all/themes/ukabbalah/page--landing-page.tpl.php
+++ sites/all/themes/ukabbalah/page--landing-page.tpl.php
@@ -10,69 +10,72 @@
 			<img src="/sites/all/themes/ukabbalah/images/title.png" alt="">
 		</div>
 		<div id="journeyContentWrpper">
-			<div id="journeyContentText">
-				<div class="sharethisWrapperLanding">
-					<span class='st_sharethis_hcount' displayText='ShareThis'></span>
-					<span class='st_facebook_hcount' displayText='Facebook'></span>
-					<span class='st_twitter_hcount' displayText='Tweet'></span>
-					<span class='st_email_hcount' displayText='Email'></span>
-				</div>
-				<p>
-					<img class="KarenImg" src="/sites/all/themes/ukabbalah/images/karen_berg-journey.jpg" alt="">
-<span class="highlightText">Many of us consider ourselves to be spiritual people - yet we continue to experience the same patterns over and over again. We try to do all the right things, yet our lives stay the same...</span>	
-					
-<p>All too often we focus solely on changing our behavior- chopping down the proverbial apple tree in hopes that an orange tree will grow. But as 
-we have learned, true change can only occur by clearing and cleaning at the seed level - in the cause, not in the effect.</p>
-	
+			<div id="journeyContent">
 
-				<p>Now more than ever, spiritual technologies from different pathways are being revealed to the masses, empowering each of us to change ourselves in real and meaningful ways.</p>
-				<p style="margin-top: 0px; margin-bottom: 0px;">
-<img alt="" src="/sites/all/themes/ukabbalah/images/yellow_message.jpg" style="margin-bottom: 20px; margin-top: 15px;">
-</p>
-				<p>
-				When we release energetic blockages from our past, we increasingly open ourselves to the endless world where the beneficent, mighty, steadfast energy of the universe exists. By cleaning and clearing we become an open channel to receive that Light.
-				</p>
-				<p>All roads lead to the endless. We are going to get there - lets get there together.</p>
-				<p><b>Its easy and free to join us. Register Now.</b></p>
-				<div id="registerHereWrapper">
-					<a target="_blank" href="https://idp.kabbalah.com/registration/meditation"><img class="register_bottom" src="/sites/all/themes/ukabbalah/images/register_bottom.png" alt=""></a>
-					<a target="_blank" href="https://idp.kabbalah.com/user/login"><img src="/sites/all/themes/ukabbalah/images/login_bottom.png" alt=""></a>
+				<div id="journeyContentText">
+					<div class="sharethisWrapperLanding">
+						<span class='st_sharethis_hcount' displayText='ShareThis'></span>
+						<span class='st_facebook_hcount' displayText='Facebook'></span>
+						<span class='st_twitter_hcount' displayText='Tweet'></span>
+						<span class='st_email_hcount' displayText='Email'></span>
+					</div>
+					<p>
+						<img class="KarenImg" src="/sites/all/themes/ukabbalah/images/karen_berg-journey.jpg" alt="">
+	<span class="highlightText">Many of us consider ourselves to be spiritual people - yet we continue to experience the same patterns over and over again. We try to do all the right things, yet our lives stay the same...</span>	
+						
+	<p>All too often we focus solely on changing our behavior- chopping down the proverbial apple tree in hopes that an orange tree will grow. But as 
+	we have learned, true change can only occur by clearing and cleaning at the seed level - in the cause, not in the effect.</p>
+		
+
+					<p>Now more than ever, spiritual technologies from different pathways are being revealed to the masses, empowering each of us to change ourselves in real and meaningful ways.</p>
+					<p style="margin-top: 0px; margin-bottom: 0px;">
+	<img alt="" src="/sites/all/themes/ukabbalah/images/yellow_message.jpg" style="margin-bottom: 20px; margin-top: 15px;">
+	</p>
+					<p>
+					When we release energetic blockages from our past, we increasingly open ourselves to the endless world where the beneficent, mighty, steadfast energy of the universe exists. By cleaning and clearing we become an open channel to receive that Light.
+					</p>
+					<p>All roads lead to the endless. We are going to get there - lets get there together.</p>
+					<p><b>Its easy and free to join us. Register Now.</b></p>
+					<div id="registerHereWrapper">
+						<a target="_blank" href="https://idp.kabbalah.com/registration/meditation"><img class="register_bottom" src="/sites/all/themes/ukabbalah/images/register_bottom.png" alt=""></a>
+						<a target="_blank" href="https://idp.kabbalah.com/user/login"><img src="/sites/all/themes/ukabbalah/images/login_bottom.png" alt=""></a>
+					</div>
 				</div>
-			</div>
-			<div id="journeyContentRightSide">
-				<div id="28DaysTitle">
-					<img src="/sites/all/themes/ukabbalah/images/28-days-title.png" alt="">
-				</div>
-				<div id="registerRightRail">
-					<a target="_blank" href="https://idp.kabbalah.com/registration/meditation"><img src="/sites/all/themes/ukabbalah/images/register_right_rail.png" alt=""></a>
-				</div>
-				<div id="loginRightRail">
-					<a target="_blank" href="https://idp.kabbalah.com/user/login"><img src="/sites/all/themes/ukabbalah/images/login_right_rail.png" alt=""></a>
-				</div>
-				<div id="tweeterWrapper">
-					<a href="https://twitter.com/karen_berg_" class="twitter-timeline" data-widget-id="449631919785783297">Tweets by @karen_berg_</a><script type="text/javascript">// <![CDATA[
-! function(d, s, id) {
-                            var js, fjs = d.getElementsByTagName(s)[0],
-                                p = /^http:/.test(d.location) ? 'http' : 'https';
-                            if (!d.getElementById(id)) {
-                                js = d.createElement(s);
-                                js.id = id;
-                                js.src = p + "://platform.twitter.com/widgets.js";
-                                fjs.parentNode.insertBefore(js, fjs);
-                            }
-                        }(document, "script", "twitter-wjs");
-// ]]></script>
-               
-               
-				</div>
-				<div id="facebookWrapper">
-					<div class="fb-page" data-href="https://www.facebook.com/karenbergkabbalah" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/karenbergkabbalah"><a href="https://www.facebook.com/karenbergkabbalah">Karen Berg</a></blockquote></div></div>
-					
-				</div>
-				<div id="loginRightRail">
-					<a href="http://www.kabbalah.com/rh">
-						<img width="310" style="border: 1px solid black;" src="http://cdn1.kabbalah.com/kabbalah.com/images/articles/rh_2015.jpg">
-					</a>
+				<div id="journeyContentRightSide">
+					<div id="28DaysTitle">
+						<img src="/sites/all/themes/ukabbalah/images/28-days-title.png" alt="">
+					</div>
+					<div id="registerRightRail">
+						<a target="_blank" href="https://idp.kabbalah.com/registration/meditation"><img src="/sites/all/themes/ukabbalah/images/register_right_rail.png" alt=""></a>
+					</div>
+					<div id="loginRightRail">
+						<a target="_blank" href="https://idp.kabbalah.com/user/login"><img src="/sites/all/themes/ukabbalah/images/login_right_rail.png" alt=""></a>
+					</div>
+					<div id="tweeterWrapper">
+						<a href="https://twitter.com/karen_berg_" class="twitter-timeline" data-widget-id="449631919785783297">Tweets by @karen_berg_</a><script type="text/javascript">// <![CDATA[
+	! function(d, s, id) {
+	                            var js, fjs = d.getElementsByTagName(s)[0],
+	                                p = /^http:/.test(d.location) ? 'http' : 'https';
+	                            if (!d.getElementById(id)) {
+	                                js = d.createElement(s);
+	                                js.id = id;
+	                                js.src = p + "://platform.twitter.com/widgets.js";
+	                                fjs.parentNode.insertBefore(js, fjs);
+	                            }
+	                        }(document, "script", "twitter-wjs");
+	// ]]></script>
+	               
+	               
+					</div>
+					<div id="facebookWrapper">
+						<div class="fb-page" data-href="https://www.facebook.com/karenbergkabbalah" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/karenbergkabbalah"><a href="https://www.facebook.com/karenbergkabbalah">Karen Berg</a></blockquote></div></div>
+						
+					</div>
+					<div id="loginRightRail">
+						<a href="http://www.kabbalah.com/rh">
+							<img width="310" style="border: 1px solid black;" src="http://cdn1.kabbalah.com/kabbalah.com/images/articles/rh_2015.jpg">
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
