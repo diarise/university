0a123414856b29fbbb64f53e79e0370d00baf8d9
Daouda Dia <diarise@gmail.com>
2015-08-06 09:24:33 -0700

    updated text on marketing page

--- sites/all/themes/ukabbalah/page--marketing-courses.tpl.php
+++ sites/all/themes/ukabbalah/page--marketing-courses.tpl.php
@@ -1,9 +1,7 @@
 <div id="bannerMarketingPage">
 	<div class="bannerMarketingPageContent">
-		<div class="bannerMarketingPageContentText">Welcome to our starter courses for the Kabbalah wisdom </div>
-		<span>Start off your journey with the Kabbalah by choosing 
-		one of our FREE courses below tailor made to your interest. 
-		No obligations.</span>
+		<div class="bannerMarketingPageContentText">Welcome teachers!</div>
+		<span>The following are 6 groups of themed content that you can share as you work with potential students to create a study plan.  The content is free of charge to the student.   If you would like to share with someone please DO NOT share URL of this page.  Click on the group that you would like to share with them and then copy the URL for that page.    The person will only have access to the videos within this group and not the other groups.  We encourage you to only share one set of videos with them as a tool to start them on their study path.</span>
 		<img src="sites/all/themes/ukabbalah/images/arrow-bottom.jpg" alt="">
 	</div>
 </div>
