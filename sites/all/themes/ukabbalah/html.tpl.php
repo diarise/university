7497d1109afce23312198b52c2d166e707ac52a2
rohandsouza <rohan.dsouza@kabbalah.com>
2015-09-24 11:23:13 -0700

    fixed the user role data

--- sites/all/themes/ukabbalah/html.tpl.php
+++ sites/all/themes/ukabbalah/html.tpl.php
@@ -267,7 +267,7 @@
 		email: '<?php echo $user->mail;?>', // Email address
 		user_id: <?php echo $user->uid;?>,
 		created_at: <?php echo $user->created;?>, // Signup date as a Unix timestamp
-		"plan": '<?php echo $userrole; ?>, // role
+		"plan": '<?php echo $userrole; ?>', // role
 	  };
 	</script>
 	<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/bjxhjpkh';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
