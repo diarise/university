d5e5b6642b45de6eda2cbe595eee442d462401d4
rohandsouza <rohan.dsouza@kabbalah.com>
2015-10-06 12:47:54 -0700

    rearranging carousals on home page for free users

--- sites/all/themes/ukabbalah/page--front.tpl.php
+++ sites/all/themes/ukabbalah/page--front.tpl.php
@@ -1,8 +1,66 @@
-	<div id="slideshow">
-		<?php  print render($page['slideshow']);?>
-	</div> <!--end of slideshow-->
+<?php 
+	global $user;
+	foreach($user->roles as $key => $role)
+	{
+		$userrole = $role;
+	}
+?>		
+
+<div id="slideshow">
+	<?php  print render($page['slideshow']);?>
+</div> <!--end of slideshow-->
 	
+
+<?php if ( user_is_logged_in() ) { ?>
 	
+	<?php if( $userrole == 'free') { ?>
+			
+		<div id="carousal4Wrapper">
+			<?php  print render($page['fpfreelessons']);?>
+		</div> <!--end of carousal4-->
+		
+		<div id="carousal1Wrapper">
+			<?php  print render($page['fpbeginnerscourses']);?>
+		</div> <!--end of carousal1-->
+		
+		<div id="carousal3Wrapper">
+			<?php  print render($page['fprecommendedcourses']);?>
+		</div> <!--end of carousal3-->
+
+		<div id="carousal2Wrapper">
+			<?php  print render($page['fprecentlyadded']);?>
+		</div> <!--end of carousal2-->
+
+		<div id="carousal5Wrapper">
+			<?php  print render($page['liveevents'])?>
+		</div> <!--end of carousel5-->
+		
+	<?php } else {  ?>	
+	
+		<div id="carousal2Wrapper">
+		<?php  print render($page['fprecentlyadded']);?>
+		</div> <!--end of carousal2-->
+
+		<div id="carousal3Wrapper">
+			<?php  print render($page['fprecommendedcourses']);?>
+		</div> <!--end of carousal3-->
+
+		<div id="carousal1Wrapper">
+			<?php  print render($page['fpbeginnerscourses']);?>
+		</div> <!--end of carousal1-->
+
+		<div id="carousal5Wrapper">
+			<?php  print render($page['liveevents'])?>
+		</div> <!--end of carousel5-->
+		
+		<div id="carousal4Wrapper">
+			<?php  print render($page['fpfreelessons']);?>
+		</div> <!--end of carousal4-->
+
+	<?php } ?>
+	
+<?php } else {  ?>
+
 	<div id="carousal2Wrapper">
 		<?php  print render($page['fprecentlyadded']);?>
 	</div> <!--end of carousal2-->
@@ -23,4 +81,5 @@
 		<?php  print render($page['fpfreelessons']);?>
 	</div> <!--end of carousal4-->
 
-	
+
+<?php } ?>	 
