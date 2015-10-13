5e0555e03b4a1f93df90799ede845cf40db25077
umesh handore <umesh.handore@kabbalah.com>
2015-10-13 11:57:53 -0700

    added the alt tag

--- sites/all/themes/ukabbalah/views-view-field--taxonomy-term--page--field-image-cdn-link.tpl.php
+++ sites/all/themes/ukabbalah/views-view-field--taxonomy-term--page--field-image-cdn-link.tpl.php
@@ -7,7 +7,7 @@
 			if( sizeof( $row->_field_data['nid']['entity']->field_course_list) > 0  ) $course_nid = node_load($row->_field_data['nid']['entity']->field_course_list['und'][0]['nid']); 
 			if( sizeof( $row->_field_data['nid']['entity']->field_event_list) > 0  )  $course_nid = node_load($row->_field_data['nid']['entity']->field_event_list['und'][0]['nid']); 	
 			$image_url = $course_nid->field_image_cdn_link['und'][0]['value'];
-			$output = theme('imagecache_external', array('path' => $image_url ,'style_name'=> 'carousal_image', )); 
+			$output = theme('imagecache_external', array('path' => $image_url ,'style_name'=> 'carousal_image', 'alt' => '',)); 
 		} 
 	}
 	//if($output)	print "<span class = 'imagesCont'><img src ='".$output."' class = 'theImage'  alt=''/>";
