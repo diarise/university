89b73d507d727d32c5555536314afb187c3c48f5
rohandsouza <rohan.dsouza@kabbalah.com>
2015-10-14 13:46:46 -0700

    testing scripts

--- sites/all/themes/ukabbalah/html.tpl.php
+++ sites/all/themes/ukabbalah/html.tpl.php
@@ -15,8 +15,8 @@
   <?php print $head_scripts; ?>
    
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
 	<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 
   <?php print $scripts; ?>	
   <script type="text/javascript">var switchTo5x=true;</script>
