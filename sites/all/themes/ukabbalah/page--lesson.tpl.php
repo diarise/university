b2d9da127f9da824edbe57d5899d0b1c6a0e69a7
umesh handore <umesh.handore@kabbalah.com>
2015-11-30 14:29:53 -0800

    adding the redirect feature
    adding the redirect feature on lesson page

--- sites/all/themes/ukabbalah/page--lesson.tpl.php
+++ sites/all/themes/ukabbalah/page--lesson.tpl.php
@@ -129,7 +129,8 @@
 					<?php } elseif( !user_is_logged_in()) { ?>
 						
 							<span class="buttonMember"><a href="https://idp.kabbalah.com">Become a member</a></span>
-							<span class="priceLogin"><a href="https://idp.kabbalah.com/user/login">Log In</a></span> 
+							<!--<span class="priceLogin"><a href="https://idp.kabbalah.com/user/login">Log In</a></span> -->
+							<?php print get_current_url();?></span> 
 						
 					<?php } ?>
 					</div>
@@ -182,7 +183,8 @@
 					<?php } elseif( !user_is_logged_in()) { ?>
 						
 							<span class="buttonMember"><a href="http://idp.kabbalah.com">Become a member</a></span>
-							<span class="priceLogin"><a href="https://idp.kabbalah.com/user/login">Log In</a></span>
+							<!--<span class="priceLogin"><a href="https://idp.kabbalah.com/user/login">Log In</a></span>-->
+							<?php print get_current_url();?></span> 
 						
 					<?php } ?>
 					</div>
@@ -421,7 +423,8 @@
 			<div class="wrapperLoginBloc" id="wrapperPrice">
 				<div class="loginText">Join Kabbalah University to watch this video. Members get access to thousands of courses and events</div>
 				<span class="buttonMember"><a href="http://idps.kabbalah.com">Become a member</a></span>
-				<span class="priceLogin logColor"><a href="https://idp.kabbalah.com/user/login">Log In</a></span>
+				<!--<span class="priceLogin logColor"><a href="https://idp.kabbalah.com/user/login">Log In</a></span>-->
+				<?php print get_current_url();?></span> 
 			</div>
 				<?php } ?>
 		 
