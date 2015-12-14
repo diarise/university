f6b6f8507d8d029d56f9e0e3a8cfab4f6e76c114
rohandsouza <rohan.dsouza@kabbalah.com>
2015-12-14 12:14:31 -0800

    ending the lesson video preview player position greater than 120secs

--- sites/all/themes/ukabbalah/js/videopreview.js
+++ sites/all/themes/ukabbalah/js/videopreview.js
@@ -6,7 +6,7 @@ Drupal.behaviors.videoPreview = {
   attach: function (context, settings) {
 	jwplayer("jwplayer1").on('time', function(e) 
 	{	
-		if( Math.round(e.position) == '120' )
+		if( Math.round(e.position) > '120' )
 		{
 			jwplayer("jwplayer1").remove();
 			$( "#videoPreview" ).remove();
