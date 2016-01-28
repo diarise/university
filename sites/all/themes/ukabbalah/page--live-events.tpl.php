f11832a3fc15af5e42564b246e0d988f8ff45ee9
Daouda Dia <diarise@gmail.com>
2016-01-28 09:31:56 -0800

    updated css

--- sites/all/themes/ukabbalah/page--live-events.tpl.php
+++ sites/all/themes/ukabbalah/page--live-events.tpl.php
@@ -4,7 +4,11 @@
 	<?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper"><?php print render($tabs); ?></div><?php endif; ?>
 	<?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
 	<?php print render($page['content']); ?>
-
+	<div class="liveEventCollectionSection">
+	<?php print render($page['liveEventCollectionSection']); ?>
+	</div>
+	</div><!--end of maincontent-->
+</main><!-- end of pageregion -->
 
 	<div class='commentsSection'>
 		<div class="commentingText">Please share your thoughts with our online student community. 
@@ -13,14 +17,3 @@
 		</div>
 		<?php print render($page['comments']); ?>
 	</div>
-
-	<div class="liveEventCollectionSection">
-	<?php print render($page['liveEventCollectionSection']); ?>
-	</div>
-	</div><!--end of maincontent-->
-</main><!-- end of pageregion -->
-
-<!-- <div class='commentsSection'>
-	
-	<?php //print render($page['comments']); ?>
-</div>  -->
