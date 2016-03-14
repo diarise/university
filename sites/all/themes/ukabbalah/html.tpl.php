bf79163ac97652b2e30cc2a34611613ea1f0de94
rohandsouza <rohan.dsouza@kabbalah.com>
2016-03-14 10:53:09 -0700

    removed zendesk from html.tpl.php

--- sites/all/themes/ukabbalah/html.tpl.php
+++ sites/all/themes/ukabbalah/html.tpl.php
@@ -192,22 +192,6 @@
 
 <?php } ?>
 	
-<script type="text/javascript" src="//assets.zendesk.com/external/zenbox/v2.6/zenbox.js"></script>
-<style>
-  @import url(//assets.zendesk.com/external/zenbox/v2.6/zenbox.css);
-</style>
-<script type="text/javascript">
-  if (typeof(Zenbox) !== "undefined") {
-    Zenbox.init({
-      dropboxID:   "20138384",
-      url:         "https://kabbalah.zendesk.com",
-      tabTooltip:  "Support",
-      tabImageURL: "https://assets.zendesk.com/external/zenbox/images/tab_support_right.png",
-      tabColor:    "black",
-      tabPosition: "Right"
-    });
-  }
-</script>
 <script type="text/javascript">
 	(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;
 		s.src='https://widget.intercom.io/widget/bjxhjpkh';
