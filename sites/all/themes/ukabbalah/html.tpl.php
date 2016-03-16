9605a51ecf87b7f5bffef548c5ecd1938e23d175
Daouda Dia <diarise@gmail.com>
2016-03-16 14:28:44 -0700

    updated og description

--- sites/all/themes/ukabbalah/html.tpl.php
+++ sites/all/themes/ukabbalah/html.tpl.php
@@ -14,7 +14,7 @@
   	<title><?php if (isset($head_image)) { print ("Kabbalah University | ");  print $head_title; } else { print $head_title;} ?></title>
   	<meta name="description" content="<?php if (isset($head_image)) { echo strip_tags($head_desc); } else{ echo "Kabbalah University is an powerful tool for spiritual connection, study and supporting a community of thousands of students around the world achieve greater spiritual growth through the wisdom of Kabbalah."; } ?>" />
   	<meta property="og:title" content="<?php print $head_title; ?>" />
-  	<meta property="og:description" content="<?php if (isset($head_image)) { echo strip_tags($head_desc); } else{ echo "Kabbalah University is an powerful tool for spiritual connection, study and supporting a community of thousands of students around the world achieve greater spiritual growth through the wisdom of Kabbalah."; } ?>" />
+  	<meta property="og:description" content="<?php if (isset($head_desc)) { echo strip_tags($head_desc); } else{ echo "Kabbalah University is an powerful tool for spiritual connection, study and supporting a community of thousands of students around the world achieve greater spiritual growth through the wisdom of Kabbalah."; } ?>" />
 	<?php if(isset($head_image)) { ?><meta property="og:image" content="<?php print $head_image; ?>" /><?php } ?>
 		
   	<?php print $styles; ?>
