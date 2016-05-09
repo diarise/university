efeed4a8bdaac0f63ee92bedf2c6a1354ee6ff53
Lu Zheng <lu.zheng@kabbalah.com>
2016-05-09 16:12:35 -0700

    test new segment implementation

--- sites/all/themes/ukabbalah/html.tpl.php
+++ sites/all/themes/ukabbalah/html.tpl.php
@@ -182,13 +182,41 @@
 		analytics.page()
 		}}();
 
-	analytics.identify('<?php echo $user->uid;?>', {
+		analytics.identify('<?php echo $user->uid;?>', {
 		  email: '<?php echo $user->mail;?>',
 		  name: '<?php echo $user->name;?>',
 		  plan: '<?php echo $userrole; ?>',
 		  createdAt: <?php echo $user->created;?>,
 		  last_seen_at: '<?php echo $user->access; ?>'
 		});
+
+		var user_id = '<?php echo $user->uid;?>'
+		var _link = "https://idp.kabbalah.com/user_profile_by_id/" + user_id;
+		
+		makeRequest(_link);
+
+		function makeRequest(url) {
+			var httpRequest = new XMLHttpRequest();
+
+	    if (!httpRequest) {
+	      console.log('Cannot create an XMLHTTP instance');
+	      return false;
+	    }
+
+	    httpRequest.onreadystatechange = alertContents;
+	    httpRequest.open('GET', url);
+	    httpRequest.send();
+
+	    function alertContents() {
+		    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+		      if (httpRequest.status === 200) {
+		        console.log(httpRequest.responseText);
+		      } else {
+		        console.log('There was a problem with the request.');
+		      }
+		    }
+			}
+ 	  } 
 	</script>
 	<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/bjxhjpkh';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
 
