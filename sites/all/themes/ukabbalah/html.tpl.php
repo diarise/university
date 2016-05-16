c09dfe6e132598a4ba1e23ca59e0f3388429ab43
Daouda Dia <diarise@gmail.com>
2016-05-16 13:18:03 -0700

    Twitter cards integration
    added twitter card meta tag

--- sites/all/themes/ukabbalah/html.tpl.php
+++ sites/all/themes/ukabbalah/html.tpl.php
@@ -12,6 +12,9 @@
 <head>
   	<?php print $head; ?>
   	<title><?php if (isset($head_image)) { print ("Kabbalah University | ");  print $head_title; } else { print $head_title;} ?></title>
+  	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="@ukabbalah" />
+	<meta name="twitter:creator" content="@ukabbalah" />
   	<meta name="description" content="<?php if (isset($head_desc)) { echo strip_tags($head_desc); } else{ echo "Kabbalah University is an powerful tool for spiritual connection, study and supporting a community of thousands of students around the world achieve greater spiritual growth through the wisdom of Kabbalah."; } ?>" />
   	<meta property="og:title" content="<?php print $head_title; ?>" />
   	<meta property="og:description" content="<?php if (isset($head_desc)) { echo strip_tags($head_desc); } else{ echo "Kabbalah University is an powerful tool for spiritual connection, study and supporting a community of thousands of students around the world achieve greater spiritual growth through the wisdom of Kabbalah."; } ?>" />
